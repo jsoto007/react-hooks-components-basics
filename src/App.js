e15a6b47e5717bb74a2c3fa447a4ffbc27bba917
Jose Soto <jsoto7087@gmail.com>
2022-08-10 02:04:44 -0400

    not sure if completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Article from "./Article";
 import Comment from "./Comment";
+import About from "./About";
 
 function App() {
   return (
     <div>
       <Article />
       <Comment />
+      <About />
     </div>
   );
 }
